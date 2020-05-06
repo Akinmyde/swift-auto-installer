c928cdba827041d798276d2e5738c10d14713dae
Akinremi Olumide <akinremiolumide96@gmail.com>
2020-05-06 16:44:02 +0100

    add documentation

--- README.md
+++ README.md
@@ -1,1 +1,51 @@
-# npm-auto-instaler
+# swift-auto-installer
+
+Auto watch and install dependencies when your teammate add changes to the package.json
+
+# Install
+
+```sh
+npm install swift-auto-installer --save-dev
+```
+
+# Usage
+
+## CLI Usage
+```sh
+run swift-watch from your terminal
+```
+
+## Hooking with your server
+
+It can be used with nodemon or node to check your packages before starting the server to do that add the following to your package.json
+```js
+// package.json
+"start": "swift-watch && node server.js"
+```
+
+## Hooking with test
+
+```js
+// package.json
+"test": "swift-watch && test server.js" // change this to your test script
+```
+
+## Using with husky
+```js
+// package.json
+{
+  "husky": {
+    "hooks": {
+      "pre-push": "swift watch",
+      "...": "..."
+    }
+  }
+}
+```
+
+## Options
+`--yarn`    Use [yarn](https://yarnpkg.com) instead of npm
+
+#### Show your support
+
+:star: this repo
--- src/index.js
+++ src/index.js
@@ -1,9 +1,28 @@
 const fs = require('fs');
 const chokidar = require('chokidar');
 const {execSync} = require('child_process');
-const chalk = require('chalk')
+const chalk = require('chalk');
+const readPkgUp = require('read-pkg-up');
 
-const isPackageJSONExist = () => fs.existsSync('package.json');
+let previouslyInstalledPackages;
+
+if(!fs.existsSync('package.json')) {
+    console.log(chalk.red("package.json file isn't present"))
+    console.log(chalk.red("try running npm init -y to create one"))
+    return;
+}
+
+const getInstalledPackages = async () => {
+    const packages = await readPkgUp();
+    return `{ "dependencies": ${JSON.stringify(packages.packageJson.dependencies)}, "devDependencies": ${JSON.stringify(packages.packageJson.devDependencies)}}`
+}
+
+const copyPackages = async () => {
+    let packages = await getInstalledPackages();
+    fs.writeFileSync('src/installed-packages.json', packages)
+    previouslyInstalledPackages = fs.readFileSync('src/installed-packages.json');
+}
+
 
 const getPackageManager = () => {
     let packageManager = 'npm';
@@ -11,7 +30,7 @@ const getPackageManager = () => {
         const file = fs.readFileSync('yarn.lock');
         if (file) {
             packageManager = 'yarn'
-            console.log('npm is detected as the default package manager')
+            console.log('yarn is detected as the default package manager')
         }
     } catch (ex) {
         console.log('npm is detected as the default package manager');
@@ -31,20 +50,29 @@ const updateNodeModules = () => {
     return isSuccessful;
 }
 
-const runner = () => {
-    console.log(chalk.green("watching......"))
-    if(!isPackageJSONExist()) {
-        console.log(chalk.red("package.json file isn't present"))
-        console.log(chalk.red("try running npm init -y to create one"))
-        return;
-    }
+const start = async () => {
+    await copyPackages();
+    console.log(chalk.green("watching......"));
 
-    let watcher = chokidar.watch('package.json');
-    watcher.on('change', path => {
-        const success = updateNodeModules();
-        if (success) console.log(chalk.green('node_modules updated'))
-        else console.log(chalk.red('an error occurred while updating node_modules'))
-    })
+    let watcher = chokidar.watch('package.json', { interval: 1000 });
+    watcher.on('change', async () => {
+        let packages = await getInstalledPackages();
+        previouslyInstalledPackages = JSON.stringify(JSON.parse(previouslyInstalledPackages));
+        packages = JSON.stringify(JSON.parse(packages))
+        if (previouslyInstalledPackages !== packages)
+        {
+            const success = updateNodeModules();
+            console.log(success);
+            if (success) {
+                 console.log(chalk.green('node_modules updated'))
+                 await copyPackages();
+                 return console.log(chalk.green("watching......"))
+            }
+            return console.log(chalk.red('an error occurred while updating node_modules'))
+        };
+        console.log(chalk.blueBright("no changes found"))
+        console.log(chalk.green("watching......"));
+    });
 }
 
-runner()
+start()
